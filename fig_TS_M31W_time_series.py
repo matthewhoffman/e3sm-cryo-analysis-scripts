c3df6ee3dbb4feb4bbed5a49e3163ccca381fd82
Carolyn Begeman <cbegeman@lanl.gov>
2023-05-25 13:47:01 -0500

    Add fig for TS time series at M31W

---
+++ fig_TS_M31W_time_series.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+'''
+Script to compare some scalar values from different runs of Thwaites melt variability experiment.
+'''
+
+import sys
+import os
+import netCDF4
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal
+from matplotlib import cm
+from math import pi
+import weddell_mod as wed
+from region_config import M31W_T_obs, M31W_S_obs
+from data_config import runname
+
+run_list = runname
+placename = ['M31W']
+year_range = [0, 200]
+input_filename = ['ISMF_ISMF-3dGM_T_S_M31W_z20mab_t001-200']
+zab = [True for i in run_list]
+zeval = [20. for i in run_list]
+wed.tseries1(run_list, ['T'], year_range=year_range,
+             input_filename=input_filename,
+             placename=placename, zeval=zeval, zab=zab,
+             print_to_file=False, create_figure=True,
+             show_obs=True, obs=M31W_T_obs,
+             year_overlay=False, overwrite=False)
+
+wed.tseries1(run_list, ['S'], year_range=year_range,
+             input_filename=input_filename,
+             placename=placename, zeval=zeval, zab=zab,
+             print_to_file=False, create_figure=True,
+             show_obs=True, obs=M31W_S_obs,
+             year_overlay=False, overwrite=False)
