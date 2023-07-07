f59360addb98b2dce1010888fa4d3c5195283c92
Carolyn Begeman <cbegeman@lanl.gov>
2023-07-07 14:25:35 -0500

    Fixup gyre interior rho tseries

--- G-ISMF_figs_gyre_int_rho.py
+++ G-ISMF_figs_gyre_int_rho.py
@@ -13,16 +13,16 @@ import scipy.signal
 from matplotlib import cm
 from math import pi
 import weddell_mod as wed
-from plot_config import gyre_interior_drho_obs, savepath_anvil
+#from plot_config import gyre_interior_drho_obs, savepath_anvil
 
-run_incr = ['ISMF','ISMF-3dGM']
+#run_incr = ['ISMF','ISMF-3dGM']
 placename = 'gyre_interior'
 var_incr = ['rho','rho']
-rholim = [1027.2,1027.9]
-filename = 'ISMF_ISMF-3dGM_rho_rho_gyre_interior_abovepyc_belowpyc_t001-200.txt'
-filename_DIB='ISMF-noDIB_data/ISMF_ISMF-noEAIS_ISMF-3dGM_ISMF-noDIB_rho_gyre_interior_abovepyc_t010-180.txt'
+rholim = [1027.2, 1027.9]
+filename = 'ISMF_ISMF-3dGM_rho_rho_gyre_interior_abovepyc_belowpyc_t001-200'#.txt'
+filename_DIB='ISMF_ISMF-noEAIS_ISMF-3dGM_ISMF-noDIB_rho_gyre_interior_abovepyc_t010-180'#.txt'
 print(filename)
-year_range = [10,200]
+year_range = [25,200]
 
 # Create timeseries
 #wed.tseries1(['ISMF','ISMF-3dGM'], var_incr,
@@ -34,12 +34,16 @@ year_range = [10,200]
 #             savepath=savepath_anvil, overwrite=False)
 
 # Plot timeseries
-wed.tseries1(['ISMF-3dGM','ISMF','ISMF-noDIB'], ['rho'],
+wed.tseries1(['VGM-DIB','CGM-DIB','CGM-UIB'], ['rho'],
              year_overlay=False, year_range=year_range,
              placename=[placename], varlim=False,
              print_to_file=False, create_figure=True,
              input_filename=[filename,filename,filename_DIB],
-             ztop_pyc=[True], zbottom_pyc=[False], 
-             diff_pyc=[True], #reference_run='ISMF', 
-             show_obs=True, obs=gyre_interior_drho_obs,
-             savepath=savepath_anvil, overwrite=False)
+             #ztop_pyc=[True],
+             ztop_pyc=[False],
+             #zbottom_pyc=[False], 
+             zbottom_pyc=[True], 
+             diff_pyc=[False], #reference_run='ISMF', 
+             #show_obs=True, obs=gyre_interior_drho_obs,
+             #savepath=savepath_anvil,
+             overwrite=False)
