f8c723fb73e9d2c4b1933c53e4eb32adcd16c4f2
Matthew Hoffman <mhoffman@lanl.gov>
2024-03-04 11:39:28 -0600

    Add velo transect too

--- fig_cross_shelf_transect.py
+++ fig_cross_shelf_transect.py
@@ -8,12 +8,12 @@ import mpas_analysis.shared.plot.inset as inset
 # full periods for production plots
 runs=['CGM-DIB', 'VGM-DIB']; years = [191]; year_range=np.arange(191,201,1)
 #runs=['CGM-UIB',]; years = [91]; year_range=np.arange(91,101,1)
-months = np.arange(1,13)
 
 # short periods for testing
 #runs=['CGM-DIB', 'VGM-DIB']; years = [191]; year_range=np.arange(191,192,1)
 #runs=['CGM-UIB',]; years = [91]; year_range=np.arange(91,92,1)
-#months = [1]
+
+#months = np.arange(1,13)
 
 rho_levels = np.arange(1027.2, 1028.0, 0.05)
 rho_levels_label = np.linspace(27.2, 28.0, 9)
@@ -32,4 +32,14 @@ for run in runs:
                  cntr_label_levels=rho_levels_label,
                  zlim=[5,-1e3], figure_format='pdf')
 
-    
+    fig = wed.transect('coord', years, months, ['u'],
+                 run=run, transect_name='trough_crossshelf',
+                 plot_transect_on_map=True,
+                 year_range=year_range,
+                 month_range=np.arange(1,13,1),
+                 ops=['time_mean'],
+                 normal=True,
+                 overwrite=True,
+                 var_contour='rho', cntr_levels=rho_levels,
+                 cntr_label_levels=rho_levels_label,
+                 zlim=[5,-1e3], figure_format='pdf')
